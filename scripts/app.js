b12634dc3a98f8fbd83fbb0c45ecd72c916db2ed
Saveliy <saveliydarovskikh@yandex.ru>
2025-09-16 18:49:48 +0500

    Не доделал, нужно доадаптировать по размерам между десктопом и мобилкой и доделать всплывающее окно на мобилке при нажатии на бургер кнопку

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 30,
+  loop: true,
+  allowTouchMove: false,
+  navigation: {
+    prevEl: '.swiper__button-prev',
+    nextEl: '.swiper__button-next',
+  },
+})
